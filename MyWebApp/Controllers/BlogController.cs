5600a459e279ef7bf3adccd239552001dfb66a59
Gaurav Sarawgi <gsarawgi@1worldsync.com>
2022-09-23 09:14:55 +0530

    Added ASP.Net Core basic web app

---
+++ MyWebApp/Controllers/BlogController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Models;
+
+namespace MyWebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        IConfiguration _config;
+
+        public BlogController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            string loggingLevel = _config.GetSection("Logging").GetSection("LogLevel").GetValue<string>("Microsoft.AspNetCore");
+            Person p1 = new Person()
+            {
+                Name = loggingLevel,
+                Age = 34,
+                Id = 7113090
+            };
+            return View(p1);
+
+        }
+    }
+}
